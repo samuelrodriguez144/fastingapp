f69d045a374fe228586703a2a33647b7d468956e
unknown <samuelfrodriguezjr@gmail.com>
2020-10-21 05:53:02 +0800

    Splash Activity and Lottie Animations

---
+++ app/src/main/java/com/guren/fastingapp/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.guren.fastingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    // Splash screen timer
+    private val SPLASH_TIME_OUT = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(i)
+            finish()
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
+    }
+}
